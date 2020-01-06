90f19b8b4309ec56157f43091c0db993ad0bf7d9
chloejay <ji.jie@edhec.com>
2020-01-07 05:24:20 +0800

    update observer

--- patterns/observer.py
+++ patterns/observer.py
@@ -1,5 +1,5 @@
 '''
-the hardcore usecase is pub/sub in event streaming system, observer pattern use the one-to-many structure. 
+the hardcore of observer pattern is pub/sub originated from event streaming concept, which uses the one-to-many structure. 
 Based on GoF book the Observer has four parts, subject, concretSubject, observer and concretObserver as 
 interface and implementation. 'encapsulate interchangable behaviors and use delegation to decide which one to use' 
 is the concept mentioned in Head First Design Pattern book. 
@@ -72,18 +72,69 @@ class ObseverFn(object):
     def fn(self,*args):
         return f'{args}' 
         
+#implement with a simple version 
+class Event:
+    _observers=list() 
+
+    def __init__(self, *args):
+        self.args= args
+
+    @classmethod 
+    def attach(cls, *observer):
+        if observer not in cls._observers:
+            cls._observers.append(*observer)
+
+    @classmethod 
+    def disattch(cls, *observer):
+        if observer in cls._observers:
+            self._observers.remove(*observer)
+
+    @classmethod 
+    def notify(cls, *subject):
+        for observer in cls._observers:
+            observer(cls(*subject)) 
+
+def observer_test(klass):
+    print('{} was written into topic'.format(klass.args))  #fn(cls(subject))
+
+class observer_test2:
+    def __call__ (self, klass):
+       print('{} was received'.format(klass.args) ) 
+
+
+def main():
+    def callObserver():
+        try:
+            pub= ConcretSubject() 
+            observer1= Observer('apple') 
+            observer2=ObserverTwo('banana')
+            pub.attach(observer1, observer1.update)
+            pub.attach(observer2, observer2.update) 
+            pub.notify('time is up!','>')
+            pub.disatch(observer2)
+            pub.disatch_all() 
+            pub.notify('kill one!','<')  
+        except AttributeError as e:
+            import logging 
+            logging.info(e) 
+            pass 
+    callObserver()
+
+    def callObserver2():
+        obj= SubjectFn() 
+        observer= ObseverFn() 
+        print(obj.attach(observer.fn('ok','viola'))) 
+    callObserver2()  
+
+    def callObserver3():
+        Event.attach(observer_test)    
+        Event.attach(observer_test2())   
+        Event.notify('new message')  
+        Event.notify('last message') 
+    callObserver3() 
+
+
 
 if __name__=='__main__':
-    pub= ConcretSubject() 
-    observer1= Observer('apple') 
-    observer2=ObserverTwo('banana')
-    pub.attach(observer1, observer1.update)
-    pub.attach(observer2, observer2.update) 
-    pub.notify('time is up!','>')
-    pub.disatch(observer2)
-    # pub.disatch_all() 
-    pub.notify('kill one!','<')  
-    obj= SubjectFn() 
-    observer= ObseverFn() 
-    print(obj.attach(observer.fn('ok','viola')))
-    
+    main() 
+
