538f1b5aec8fb0318bd385584cc29dde60e56a35
chloejay <ji.jie@edhec.com>
2020-02-07 04:07:16 +0800

    add res

--- protocal/res.py
+++ protocal/res.py
@@ -1,49 +1,58 @@
 import requests 
 from pprint import pprint 
 import simplejson as json 
+from bs4 import BeautifulSoup
+import time 
 
-#TODO 
-url='https://twitter.com/home'
-TIMEOUT= 20
-
-# data, payload  
+url='https://www.goodreads.com/search/index.xml'
+TIMEOUT= 100
+ 
 class Request(object):
-    def __init__(self, name, pswd, url = url, timeout = TIMEOUT):
+    def __init__(self, key, url = url, timeout = TIMEOUT, _secret= ''):
         self.url = url
-        # self.data = data
         self.timeout = timeout 
-        # self.payload = payload
-        self._name = name 
-        self._pswd=pswd
+        self._key = key 
+        self._secret=_secret
 
     def send_request(self):
-        r = requests.get(self.url, timeout = self.timeout,auth = (self._name, self._pswd)) 
+        r = requests.get(self.url, timeout = self.timeout,params={'key':self._key, 'q':'some_book'}) 
         # print(r.status_code) #DEBUG 
         try:
 
             if r.status_code == requests.codes.ok:
                 content = r.content 
-                pprint(content) 
-                items = json.loads(content) 
-                for item in items:
-                    # pprint(item) 
-                    return item 
+                c=json.dumps(content)
+                items = json.loads(c) 
+                bs = BeautifulSoup(items, 'html.parser')
+                # print(bs.prettify())
+                works= bs.find_all('work')
+                for w in works:
+                    
+                    authors=w.find_all('author')
+                    authorlist, ratings = [],[]
+                    for author in authors:
+                        authorlist.append(author.find('name').getText()) 
+                    ratings.append(w.find('average_rating').getText())
+
+                    zipped=zip(authorlist, ratings) 
+                    for a,r in list(zipped):
+                        print(f'author name is {a}, whose book avg rating is {r}')
+                        # return (a,r) 
+                        time.sleep(3)
+                    
             else:
                 return f'error is {r.raise_for_status()}'
 
         except Exception as e:
             pprint(str(e)) 
 
-    #TODO
-    def post_response(self):
-        r=requests.post(self.url, json = {'key', 'val'})
-        pprint(r.json()) 
-        return r.json()  
+    #TODO 
+    # def post_response(self):
+    #     r=requests.post(self.url, json = {'key', 'val'})
+    #     pprint(r.json()) 
+    #     return r.json()  
 
 if __name__=='__main__':
-
-    x= Request('chloeji91','') 
-    x.send_request() 
-
-
-
+    _key='2fUAowbKb7jmPpuIsoDqA'
+    x= Request(_key) 
+    print(x.send_request()) 
