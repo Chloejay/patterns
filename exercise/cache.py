36d9712e67e3fff9c41eadccad4fca27704669ca
Chloejay <ji.jie@edhec.com>
2020-02-25 08:25:31 +0800

    Add cache python built in lur_cache to patterns subfolder exercise

--- exercise/cache.py
+++ exercise/cache.py
@@ -1,26 +1,48 @@
 from functools import lru_cache 
-import time
+from time import time 
+from datetime import datetime 
+import logging 
+logging.basicConfig(level=logging.INFO)
 
+
+def logtime(if_cache_func):
+    def wrapper(*args, **kwargs):
+        start = time() 
+        result = if_cache_func(*args, **kwargs)
+        exc_time = time()- start
+
+        with open (f'{if_cache_func.__name__}_eventLog.txt', 'a') as outf:
+            outf.write(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")}\t{if_cache_func.__name__}\t{exc_time:.4f}\n')
+        return result 
+
+    return wrapper 
+
+@logtime
 def fib(number: int) -> int:
     if number == 0: 
         return 0
     elif number == 1: 
         return 1
     return fib(number-1) + fib(number-2)
-start = time.time()
-fib(100)
-print(f'Duration: {time.time() - start}s') 
 
-#the concept is memoization 
-@lru_cache(maxsize=512)
-def fib_1(n: int)-> int:
-    if n==0:
+
+#maxsize is size of cache, if is None, lru features are disabled and cache can grow without bound
+@lru_cache(maxsize = 32)
+@logtime
+def fib_memoization(n: int)-> int:
+    if n == 0:
         return 0
-    elif n==1:
+    elif n == 1:
         return 1
-    return fib_1(n-2)+fib_1(n-1) 
- 
-start = time.time()
-fib_1(100)
-print(f'Duration: {time.time() - start}s') 
 
+    return fib_memoization(n-1) + fib_memoization(n-2)
+
+
+def main():
+    fib(20)
+    fib_memoization(20)
+    logging.info(fib_memoization.cache_info())
+
+
+if __name__ == '__main__':
+    main() 
