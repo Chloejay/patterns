5b57a5def06ce9e6db1d1698e56bfcef217a4c8b
Chloejay <ji.jie@edhec.com>
2020-05-12 08:38:38 +0800

    add state and template.py

--- patterns/state.py
+++ patterns/state.py
@@ -1,4 +1,84 @@
 """
-change object behaviors based on state
+change object behaviors based on states
 """
 
+
+from abc import ABC, abstractmethod
+
+class Seasons(ABC):
+    # all the methods will do the same behaviors based on state
+
+    @abstractmethod
+    def commute(self):
+        pass
+
+    @abstractmethod
+    def wear(self):
+        pass
+
+    @abstractmethod
+    def eat(self):
+        pass
+
+class Summer(Seasons):
+    def __init__(self, loc: str, tool: str, cloth: str, food: str)-> None:
+
+        self.loc = loc
+        self.tool = tool
+        self.cloth = cloth
+        self.food = food
+
+    def commute(self):
+        print(f"by {self.tool} at {self.loc}")
+
+    def wear(self):
+        print(f"wear {self.cloth}")
+
+    def eat(self):
+        print(f"eat {self.food}")
+
+
+class Winter(Seasons):
+    def __init__(self, tool: str, cloth: str, food: str, loc: str = "space")-> None:
+        
+        self.loc = loc
+        self.tool = tool
+        self. cloth = cloth
+        self. food = food
+
+    def commute(self):
+        print(f"by {self.tool} at {self.loc}")
+
+    def wear(self):
+        print(f"wear {self.cloth}")
+
+    def eat(self):
+        print(f"eat {self.food}")
+
+class Person(Seasons):
+
+    def __init__(self, state: str)-> None:
+        self._state = state
+
+    def set_state(self, state: str)-> None:
+        self._state = state
+
+    def commute(self):
+        return self._state.commute()
+
+    def wear(self):
+        return self._state.wear()
+
+    def eat(self):
+        return self._state.eat()
+
+
+if __name__ == "__main__":
+    summer = Person(Summer("earth", "bike", "t-shirt", "ice cream"))
+    winter = Person(Winter("walk", "jacket", "hot pot"))
+    print(summer.commute())
+    print(summer.wear())
+    print(summer.eat())
+    print(winter.commute())
+    print(winter.wear())
+    print(winter.eat())
--- patterns/template.py
+++ patterns/template.py
@@ -1,11 +1,80 @@
 """
-when two classes share the same behaviors, the use tempate pattern to generalize to one class, abstract superclass
+when two classes share the same behaviors, then use tempate pattern to generalize to one class,
+abstract superclass
 """
 
-# TODO 
-# abstract class 
+
+from abc import ABC, abstractmethod
+
+class Result(ABC):
+    # hold template methods
+    def get_result(self):
+        self.do()
+        self.sports()
+        self.work()
+        self.relax()
+
+    # abstract method
+    @abstractmethod
+    def do(self):
+        pass
+
+    @abstractmethod
+    def sports(self):
+        pass
+
+    @abstractmethod
+    def work(self):
+        pass
+
+    @abstractmethod
+    def relax(self):
+        pass
 
 # concrete class one 
+class Person_Extrovert(Result):
+    def __init__(self, who: str, what: str, field: str, hobby: str)-> None:
+        self._who = who
+        self.what = what
+        self.field = field
+        self.hobby = hobby
+
+    def do(self):
+        print(f"{self._who} a is doing")
+
+    def sports(self):
+        print(f"{self.what} is a group sports")
+
+    def work(self):
+        print(f"work in {self.field}")
+
+    def relax(self):
+        print(f"maybe go to {self.hobby}")
 
 # concrete class two 
+class Person_Introvert(Result):
+    def __init__(self, who: str, what: str, field: str, hobby: str)-> None:
+        self._who = who
+        self.what = what
+        self.field = field
+        self.hobby = hobby
 
+    def do(self):
+        print(f"{self._who} is doing")
+    def sports(self):
+        print(f"{self.what} is a solo sports")
+
+    def work(self):
+        print(f"communicate with {self.field}")
+
+    def relax(self):
+        print(f"go for {self.hobby}")
+
+
+
+if __name__ == "__main__":
+    typeA = Person_Extrovert("extrovertA", "basketball", "communication", "ktv")
+    typeB = Person_Introvert("introvertB", "biking", "code", "hiking") 
+    typeA.get_result()
+    print("----"*20)
+    typeB.get_result()
